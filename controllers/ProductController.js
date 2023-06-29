68dd0891d0c6d88caf8c1d70c5e643926603aab0
juniorpocaygit <juniorpocay@gmail.com>
2023-06-29 07:50:20 -0300

    finalização da adequação do projeto ao mongoose

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -2,7 +2,7 @@ const Product = require('../models/Product')
 
 module.exports = class ProductController{
     static async showProducts(req, res){
-        const products = await Product.getProducts()
+        const products = await Product.find().lean()
         res.render('products/all', {products})
     }
 
@@ -10,14 +10,14 @@ module.exports = class ProductController{
         res.render('products/create')
     }
     
-    static createProductPost(req, res){
+    static async createProductPost(req, res){
         const name = req.body.name
         const image = req.body.image
         const price = req.body.price
         const description = req.body.description
 
-        const product = new Product(name, image, price, description)
-        product.save()
+        const product = new Product({name, image, price, description})
+        await product.save()
         res.redirect('/products')
 
     }
@@ -25,7 +25,7 @@ module.exports = class ProductController{
     static async getProduct(req, res){
 
         const id = req.params.id
-        const product = await Product.getProductById(id)
+        const product = await Product.findById(id).lean()
         res.render('products/product', { product })
 
     }
@@ -33,7 +33,7 @@ module.exports = class ProductController{
     static async removeProduct(req, res){
        
         const id = req.params.id
-        await Product.removeProductById(id)        
+        await Product.deleteOne({_id: id})        
         res.redirect('/products')
     
     }
@@ -41,7 +41,7 @@ module.exports = class ProductController{
     static async editProduct(req, res){
        
         const id = req.params.id
-        const product = await Product.getProductById(id)        
+        const product = await Product.findById(id).lean()        
         res.render('products/edit', { product })
     
     }
@@ -54,8 +54,8 @@ module.exports = class ProductController{
         const price = req.body.price
         const description = req.body.description
        
-        const product = new Product(name, image, price, description)       
-        await product.updateProduct(id)
+        const product = {name, image, price, description}       
+        await Product.updateOne({_id: id}, product)
 
         res.redirect('/products')
     
